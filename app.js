dbb4d11611d08169dbde25dd6b0f8af2ddc97347
Svet <125960896+akiiyamas@users.noreply.github.com>
2024-11-20 15:05:05 +0200

    app.js

--- app.js
+++ app.js
@@ -5,3 +5,29 @@ menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
 });
+
+document.getElementById('comment-form').addEventListener('submit', function (event) {
+  event.preventDefault();  
+
+  // commenter's name and comment text
+  const name = document.getElementById('name').value;
+  const comment = document.getElementById('comment').value;
+
+  if (name && comment) {
+      // Create a new div to display the comment
+      const commentDiv = document.createElement('div');
+      commentDiv.classList.add('comment');
+      
+      // comment content
+      commentDiv.innerHTML = `
+          <strong>${name}</strong>: <p>${comment}</p>
+      `;
+      
+      // new comment to the comments display section
+      document.getElementById('commentsDisplay').appendChild(commentDiv);
+      
+      
+      document.getElementById('name').value = '';
+      document.getElementById('comment').value = '';
+  }
+});
